5d92b63b30c31c3608f93510bfd876846331fcbd
liaakin <lia.07@live.de>
2026-08-12 00:05:54 +0200

    Add Footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,10 @@
+import { styles } from '../styles/global'
+import { View, Text } from 'react-native'
+
+export default function Footer() {
+  return (
+    <View style={styles.footer}>
+      <Text style={styles.footerText}>fidizle | © {new Date().getFullYear()} liascope</Text>
+    </View>
+  )
+}
