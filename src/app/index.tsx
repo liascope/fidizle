01b57ed3bb6d94fd8c5e1758f5ac891d98a831b7
liaakin <lia.07@live.de>
2026-08-15 21:29:37 +0200

    feat: complete movie and series navigation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,20 @@
-import { Text, View } from 'react-native'
-import { styles } from './styles/global'
-import HomeHeader from './components/HomeHeader'
-import Search from './components/Search'
-import { useMovies } from './hooks/useMovies'
-import { useState } from 'react'
-import MovieList from './components/MovieList'
-import Box from './components/Box'
-import SelectedMovie from './components/SelectMovie'
-import { useLocalStorage } from './hooks/useLocalStorage'
-import WatchedSummary from './components/WatchedSummary'
-import WatchedMoviesList from './components/WatchedMoviesList'
-import Loader from './components/Loader'
-import Error from './components/Error'
-import { WatchedMovie } from './components/WatchedSummary'
-import Footer from './components/Footer'
 import { LinearGradient } from 'expo-linear-gradient'
+import { Text, View } from 'react-native'
+import Box from './components/Box'
+import Error from './components/Error'
+import Loader from './components/Loader'
+import MovieList from './components/MovieList'
+import SelectedMovie from './components/SelectMovie'
+import WatchedList from './components/WatchedList'
+import { WatchedMovie } from './components/WatchedSummary'
+import { useLocalStorage } from './hooks/useLocalStorage'
+import { useMovies } from './hooks/useMovies'
+import { styles } from './styles/global'
+import { useSearch } from './context/SearchContext'
 
 export default function HomeScreen() {
-  const [query, setQuery] = useState('')
-
-  const [watched, setWatched] = useLocalStorage<WatchedMovie[]>([], 'watched')
-  const [selectedId, setSelectedId] = useState<string | null>(null)
+  const { query, selectedId, setSelectedId } = useSearch()
+  const { watched, setWatched } = useLocalStorage<WatchedMovie>([], 'watched')
   const { movies, isLoading, error } = useMovies(query)
 
   function handleSelectedMovie(id: string) {
@@ -32,20 +26,18 @@ export default function HomeScreen() {
   function handleAddWatchedMovie(movie: WatchedMovie) {
     setWatched((w) => [...w, movie])
   }
-  function handleDeleteWatched(id: string) {
-    setWatched((w) => w.filter((m) => m.imdbID !== id))
-  }
 
   return (
     <LinearGradient colors={['#090909', '#260909', '#090909']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={styles.container}>
-      <HomeHeader>
-        <Search query={query} setQuery={setQuery}></Search>
-        <Text style={styles.results}>
-          Found <Text style={styles.numResults}>{movies?.length}</Text> results.
-        </Text>
-      </HomeHeader>
       <View style={styles.main}>
         <Box>
+          <Text style={styles.results}>
+            {query && (
+              <>
+                {movies?.length} results for <Text style={styles.numResults}>'{query}'</Text>
+              </>
+            )}
+          </Text>
           {isLoading && <Loader />}
           {!isLoading && !error && <MovieList movies={movies} handleSelectedMovie={handleSelectedMovie} />}
           {error && <Error message={error} />}
@@ -55,13 +47,12 @@ export default function HomeScreen() {
             <SelectedMovie handleCloseId={handleCloseId} selectedId={selectedId} handleAddWatchedMovie={handleAddWatchedMovie} watched={watched} />
           ) : (
             <>
-              <WatchedSummary watched={watched} />
-              <WatchedMoviesList handleDeleteWatched={handleDeleteWatched} watched={watched} />
+              <WatchedList type="movie" />
+              <WatchedList type="series" />
             </>
           )}
         </Box>
       </View>
-      <Footer />
     </LinearGradient>
   )
 }
