c5f2f9c3e283b79cebc850618d67f39438c7fa18
liaakin <lia.07@live.de>
2026-08-16 18:09:48 +0200

    feat: add global search

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,19 +4,22 @@ import HomeHeader from './components/HomeHeader'
 import Search from './components/Search'
 import Footer from './components/Footer'
 import { styles } from './styles/global'
+import { ScrollView } from 'react-native'
+
 import { SearchProvider, useSearch } from './context/SearchContext'
+import SearchResults from './components/SearchResults'
 
 function AppLayout() {
-  const { query, setQuery } = useSearch()
+  const { query, setSearchQuery } = useSearch()
 
   return (
     <LinearGradient colors={['#090909', '#260909', '#090909']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={styles.container}>
-      <HomeHeader>
-        <Search query={query} setQuery={setQuery} />
-      </HomeHeader>
-
-      <Slot />
-
+      <ScrollView style={styles.homeContent} contentContainerStyle={styles.homeContentContainer} showsVerticalScrollIndicator={false}>
+        <HomeHeader>
+          <Search query={query} setSearchQuery={setSearchQuery} />
+        </HomeHeader>
+        {query.trim().length > 0 ? <SearchResults query={query} /> : <Slot />}
+      </ScrollView>
       <Footer />
     </LinearGradient>
   )
--- src/app/components/HomeHeader.tsx
+++ src/app/components/HomeHeader.tsx
@@ -11,14 +11,12 @@ export default function HomeHeader({ children }: { children: React.ReactNode }) 
   return (
     <LinearGradient colors={['#080808', '#4A1518', '#080808']} start={{ x: 0, y: 0 }} end={{ x: 1, y: 0 }} style={styles.navBar}>
       <View style={[styles.filmHoles, styles.filmHolesTop]}>
-        {Array.from({ length: 18 }).map((_, i) => (
+        {Array.from({ length: 22 }).map((_, i) => (
           <View key={i} style={styles.filmHole} />
         ))}
       </View>
 
-      {/* Header */}
       <View style={styles.navBarContent}>
-        {/* Logo */}
         <Pressable onPress={() => router.push('/')}>
           <View style={styles.logo}>
             <Text style={styles.logoIcon}>🎬</Text>
@@ -43,7 +41,7 @@ export default function HomeHeader({ children }: { children: React.ReactNode }) 
       </View>
 
       <View style={[styles.filmHoles, styles.filmHolesBottom]}>
-        {Array.from({ length: 18 }).map((_, i) => (
+        {Array.from({ length: 22 }).map((_, i) => (
           <View key={i} style={styles.filmHole} />
         ))}
       </View>
